bfd48c0c7555f6260694b48db8d3608a9b795d70
wiktordziadosz <szymonwik99gmail.com>
2022-04-20 21:58:21 +0200

    deploying

---
+++ carspottingapp/deploy.sh
@@ -0,0 +1,12 @@
+// building for productiion and generating dist file with static content
+npm run build
+// change into dist directory
+cd dist
+// commiting changes
+git init
+git add -A
+git commit -m "dist commit"
+// pushing changes on gh-pages branch 
+git push -f https://github.com/DziadoszWiktor/CarSpottingApp.git master:gh-pages
+// returning to project directory
+cd ..   
