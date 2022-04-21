c5ae339715bb7f1b38fd812b58ef4fbed56f9dfd
wiktordziadosz <szymonwik99gmail.com>
2022-04-21 20:01:38 +0200

    pwa

--- carspottingapp/deploy.sh
+++ carspottingapp/deploy.sh
@@ -1,12 +1,12 @@
-// building for productiion and generating dist file with static content
+#building for productiion and generating dist file with static content
 npm run build
-// change into dist directory
+#change into dist directory
 cd dist
-// commiting changes
+#commiting changes
 git init
 git add -A
 git commit -m "dist commit"
-// pushing changes on gh-pages branch 
+#pushing changes on gh-pages branch 
 git push -f https://github.com/DziadoszWiktor/CarSpottingApp.git master:gh-pages
-// returning to project directory
+#returning to project directory
 cd ..   
