75f6ad667d84a3a8d24658a3407dcb7caafcb826
wiktordziadosz <szymonwik99gmail.com>
2022-04-21 20:00:34 +0200

    pwa

--- carspottingapp/vue.config.js
+++ carspottingapp/vue.config.js
@@ -3,5 +3,14 @@ module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
     ? '/CarSpottingApp/'
-    : '/'
+    : '/',
+  pwa: {
+    name: 'Car Spotting App',
+    short_name: 'Car Spotting App',
+    themeColor: 'black',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black'
+    // other PWA manifest configuration here...
+  }
 })
