c0094dea64efdaa3ab52c2fc7d676c1a32d599d1
wiktordziadosz <szymonwik99gmail.com>
2022-04-26 21:46:46 +0200

    please

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,5 @@
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
-  ],
-  loaders: [
-    { test: /\.js$/, loader: 'babel', query: {compact: false} }
-]
+  ]
 }
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,41 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+    module.exports = defineConfig({
+        transpileDependencies: true,
+        publicPath: process.env.NODE_ENV === 'production'
+            ? '/gh-pages-vue-deployment/'
+            : '/',
+        pwa: {
+            name: 'gh-pages Vue PWA App deployment ',
+            short_name: 'Vue PWA',
+            themeColor: 'black',
+            msTileColor: '#000000',
+            appleMobileWebAppCapable: 'yes',
+            appleMobileWebAppStatusBarStyle: 'black',
+            color: 'white',
+            background_color: 'black'
+            /*
+            // configure the workbox plugin
+            workboxPluginMode: 'InjectManifest',
+            workboxOptions: {
+            // swSrc is required in InjectManifest mode.
+            swSrc: 'dev/sw.js',
+            // ...other Workbox options...
+            }*/
+        },
+        chainWebpack: config => {
+          config.performance
+            .maxEntrypointSize(8000000)
+            .maxAssetSize(8000000)
+      }
+    })
+
+
+    /*
+
+    chainWebpack: config => {
+            config.performance
+              .maxEntrypointSize(5000000)
+              .maxAssetSize(5000000)
+        }
+
+    */
