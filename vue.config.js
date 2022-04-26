2e65e13297e9f364bd73690df2cf13968a21ba22
wiktordziadosz <szymonwik99gmail.com>
2022-04-26 22:21:16 +0200

    pleasepart2

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
     module.exports = defineConfig({
         transpileDependencies: true,
         publicPath: process.env.NODE_ENV === 'production'
-            ? '/gh-pages-vue-deployment/'
+            ? '/CarSpottingApp/'
             : '/',
         pwa: {
             name: 'gh-pages Vue PWA App deployment ',
